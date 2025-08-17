7b0adbffed555e1734e5c47ff8e587a97b3ff4a9
Wen Ni Lim <wenni1999@gmail.com>
2025-08-17 17:02:41 +0800

    add enable CORS origin

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,15 @@ async function bootstrap() {
   // 如果要globally把所有API只是指定给admin就那么写
   // const jwtService = app.get(JwtService);
   // app.useGlobalGuards(new AdminGuard(jwtService));
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // 本地开发 React
+      'https://nova-panel.netlify.app', // 线上部署的前端
+    ],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
